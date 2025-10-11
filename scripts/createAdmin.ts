5c1ee22a08f73bf3eca02ec57b8943b321b2ffc7
hoaug-tran <92006941+hoaug-tran@users.noreply.github.com>
2025-10-11 19:39:45 +0700

    chore: createAdmin.ts

--- scripts/createAdmin.ts
+++ scripts/createAdmin.ts
@@ -6,18 +6,18 @@ import db from "../src/config/db.js";
 const createAdmin = async () => {
   const id = uuidv4();
   const PEPPER = process.env.PASSWORD_PEPPER || "";
-  const hash = await bcrypt.hash("Kinhhoang@2" + PEPPER, 10);
+  const hash = await bcrypt.hash("123456" + PEPPER, 10);
 
   await db.query(
     `
     INSERT INTO users (id, studentId, full_name, email, password, role, status)
     VALUES (?, NULL, ?, ?, ?, 'ADMIN', 'ACTIVE')
     `,
-    [id, "Trần Kính Hoàng", "hoaug@duck.com", hash]
+    [id, "Nguyễn Văn A", "123456", hash]
   );
 
   console.log("Đã tạo thành công tài khoản Admin:");
-  console.log({ email: "hoaug@duck.com", password: "Kinhhoang@2" });
+  console.log({ email: "hoaug@xyz.com", password: "123456" });
   process.exit(0);
 };
 
