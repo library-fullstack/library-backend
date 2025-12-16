6d06b8a9052b3a89eea479d8d47b26d6a8c7e8f6
unknown <hoaug@duck.com>
2025-12-16 16:19:35 +0700

    feat: add logic and forum page

--- migrations/007_add_likes_count_to_comments.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add likes_count column to forum_comments table
--- Since we removed forum_comment_likes table, we need likes_count denormalized column
-
-ALTER TABLE forum_comments 
-ADD COLUMN likes_count INT DEFAULT 0 NOT NULL;
-
--- Verify
-SELECT 'forum_comments.likes_count column added' as status;
